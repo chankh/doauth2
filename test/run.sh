3b1b8617c95223292acf7975b4d42c374250de85
KH Chan <khchan@v-cumulus.com>
2013-09-26 18:23:02 +0800

    removed dart analyzer

--- test/run.sh
+++ test/run.sh
@@ -12,19 +12,3 @@ if [ $EXITCODE -ne 0 ]; then
     exit $EXITCODE
 fi
 
-#####
-# Type Analysis
-
-echo
-echo "dartanalyzer lib/*.dart"
-
-results=`dartanalyzer lib/*.dart 2>&1`
-EXITCODE=$?
-
-echo "$results"
-
-if [ $EXITCODE -ne 0 ]; then
-    exit $EXITCODE
-else
-    echo "Passed analysis."
-fi
